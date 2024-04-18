b01e322fbdc0148e7e93f7ab8734432d7bf11995
dgdgdgdg <62982921+dg4-design@users.noreply.github.com>
2024-04-18 14:05:54 +0900

    update renewal firstview

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,25 @@ Array.from(navLink).forEach((link) => {
     header.classList.toggle("active");
   });
 });
+
+const topTitleElement = document.getElementById("topTitle");
+
+document.addEventListener("mousemove", (event) => {
+  const widthPercentage = (event.clientX / window.innerWidth) * 100;
+  const heightPercentage = (event.clientY / window.innerHeight) * 50;
+  if (topTitleElement) {
+    topTitleElement.style.fontVariationSettings = `'rnds' ${widthPercentage}, 'dtsz' ${heightPercentage + 50}`;
+  }
+});
+
+const adjustFontSize = () => {
+  if (topTitleElement) {
+    const screenWidth = window.innerWidth;
+    const textLength = topTitleElement.textContent.length;
+    const fontSize = (screenWidth * 2) / textLength;
+    topTitleElement.style.fontSize = `${fontSize}px`;
+  }
+};
+
+window.addEventListener("load", adjustFontSize);
+window.addEventListener("resize", adjustFontSize);
